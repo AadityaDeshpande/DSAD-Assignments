7cf9feb6a92c10e5a0fcee96cf69336b4885871e
Aaditya Deshpande <aadityadeshpande1998@gmail.com>
2022-06-18 11:18:52 +0530

    Code Completion

--- 21.py
+++ 21.py
@@ -1,9 +1,26 @@
 from sys import exit
 
-# Reading the Input file inputPS07.txt
-# Populate transaction_list to create binary tree
-# Populate actions_list to perform actions on the binary tree
+""" 
+Reading the Input file inputPS07.txt
+Populate transaction_list to create binary tree
+Populate actions_list to perform actions on the binary tree
+"""
 inputFileName: str = "inputPS07.txt"
+outputFileName: str = "outputPS07.txt"
+
+# Redirect the print outputs to the file
+class FileWriter:
+    """ A File writer class """
+    file = open(outputFileName, 'a')
+    def __init__(self):
+        self.file = open(outputFileName, 'a')
+    
+    def __del__(self):
+        self.file.close()
+
+    @classmethod
+    def write_in_file(cls, content):
+        cls.file.write(content + "\n")
 
 try:
     with open(inputFileName, 'r') as infile:
@@ -12,17 +29,19 @@ try:
         Employee_transactions_list: list = [int(i) for i in lines if i.isdigit()]
         actions_list: list = [i for i in lines if not i.isdigit()]
 except FileNotFoundError as e:
-    print(f"Input file {inputFileName} is not present in the same directory \n{e}")
+    FileWriter.write_in_file(f"Input file {inputFileName} is not present in the same directory \n{e}")
     exit(0)
 
 # validation for no employees
 if not len(Employee_transactions_list):
-    print("Can't create a binary tree without any elements")
+    FileWriter.write_in_file("Can't create a binary tree without any elements")
     exit(0)
 
 # Construct a binary tree
-# Each node represents an employee
-# attctr represents the number of times employee swipped a card
+""" 
+Each node represents an employee
+attctr represents the number of times employee swipped a card
+"""
 class EmpNode:
     def __init__(self, EmpId: int):
         """ Parameterised constructor """
@@ -54,17 +73,17 @@ class EmpNode:
         if self.left:
             self.left.get_status(Employee_dict, print_tree)
         if print_tree:
-            print("Employee ID:",self.EmpID, ", and attctr value :",self.attctr)
+            FileWriter.write_in_file("Employee ID:",self.EmpID, ", and attctr value :",self.attctr)
         Employee_dict.update({self.EmpID:self.attctr})
         if self.right:
             self.right.get_status(Employee_dict, print_tree)
 
 def searchEmployee(root: EmpNode, EmpID):
     if root is None:
-        print(f"Employee id {EmpID} did not swipe today.")
+        FileWriter.write_in_file(f"Employee id {EmpID} did not swipe today.")
         return root
     if root.EmpID == EmpID:
-        print(f"Employee id {root.EmpID} swiped {root.attctr} times today and is currently {'outside' if root.attctr % 2 == 0 else 'inside'} freezer room")
+        FileWriter.write_in_file(f"Employee id {root.EmpID} swiped {root.attctr} times today and is currently {'outside' if root.attctr % 2 == 0 else 'inside'} freezer room")
         return root
     if root.EmpID < EmpID:
         return searchEmployee(root.right, EmpID)
@@ -72,29 +91,51 @@ def searchEmployee(root: EmpNode, EmpID):
         return searchEmployee(root.left, EmpID)
 
 # Driver code
-# Construct a Tree
-# create a root node with first value
+"""
+Construct a Tree
+create a root node with first value
+"""
 root = EmpNode(Employee_transactions_list[0])
 
 # create a binary tree
 for EmpID in Employee_transactions_list[1:]:
     root.insert(EmpID)
 
-# cache the employee status to improve performance
-Employee_status = {}
-root.get_status(Employee_status, False)
-
 for action in actions_list:
     if 'inFreezer' in action:
         # logic to get employees in the freezer
+        Employee_status = {}
+        root.get_status(Employee_status, False)
         inside_freezer = 0
         for k, v in Employee_status.items():
             if v % 2 != 0:
                 inside_freezer += 1
-        print(f"Total number of employees recorded today: {len(Employee_status.keys())} {inside_freezer} employee(s) still inside freezer room")
+        FileWriter.write_in_file(f"Total number of employees recorded today: {len(Employee_status.keys())} {inside_freezer} employee(s) still inside freezer room")
+    
     if 'checkEmp' in action:
         emp_to_search = action.split(':')[1].strip()
         if emp_to_search.isdigit():
             emp_to_search = int(emp_to_search)
         searchEmployee(root, emp_to_search)
-    # TODO: for 'freqVisit: 3', 'range: 05:125'
+    
+    if 'freqVisit' in action:
+        frequency = action.split(':')[1].strip()
+        # logic to get employees frequency status
+        Employee_status = {}
+        root.get_status(Employee_status, False)
+        for k, v in Employee_status.items():
+            if int(frequency) <= v:
+                FileWriter.write_in_file(f"{k}, {v}")
+    
+    if 'range' in action:
+        lower_range = action.split(':')[1].strip()
+        upper_range = action.split(':')[2].strip()
+        # FileWriter.write_in_file(f"{lower_range}, {upper_range}")
+        FileWriter.write_in_file(f"Range: {lower_range} to {upper_range} Employee swipe:")
+        # logic to get employees frequency status
+        Employee_status = {}
+        root.get_status(Employee_status, False)
+
+        for k,v in Employee_status.items():
+            if k <= int(upper_range) and k >= int(lower_range):
+                FileWriter.write_in_file(f"{k}, {v}, {'out' if v % 2 == 0 else 'in'}")
